7ebad1e3357e6687cab95bc458218a893e5c481f
Matija Stepanic <stepanic@loopus.co>
2016-02-24 17:07:28 +0100

    iOS Sample app with Binding BlinkID works with init features

---
+++ Samples/iOS/ViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using System.Diagnostics;
+
+using MicroBlink;
+
+namespace iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			CustomDelegate customDelegate = new CustomDelegate ();
+
+			BlinkID.Instance().LicenseKey = "CPXEWTU6-23VZUKIW-PKZ3JZMC-ZJZJJF7N-76T7VLXP-MOPXWCKG-FJNIDWKE-VBBUL3FU";
+			BlinkID.Instance().Delegate = customDelegate;
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void StartScanningButton_TouchUpInside (UIButton sender)
+		{
+			BlinkID.Instance().Scan();
+		}
+
+		class CustomDelegate : BlinkIDDelegate
+		{
+			#region implemented abstract members of BlinkIDDelegate
+
+			public override void DidOutputResults (BlinkID blinkid, NSDictionary[] results)
+			{
+				UIAlertView alert = new UIAlertView () { 
+					Title = "BlinkID results", Message = results[0].ToString()
+				};
+				alert.AddButton("OK");
+				alert.Show ();
+			}
+
+			#endregion
+		}
+	}
+}
+
