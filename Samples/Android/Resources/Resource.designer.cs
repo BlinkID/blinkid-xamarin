ce50586d8d88bf04c3d9f36f2b497d162e5207e9
Matija Stepanic <stepanic@loopus.co>
2016-03-03 15:43:39 +0100

    Android Sample works

--- Samples/Android/Resources/Resource.designer.cs
+++ Samples/Android/Resources/Resource.designer.cs
@@ -31,6 +31,21 @@ namespace Android
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010001
+			public const int animateRotation = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int animationDuration = 2130771970;
+			
+			// aapt resource value: 0x7f010003
+			public const int aspectMode = 2130771971;
+			
+			// aapt resource value: 0x7f010000
+			public const int initialOrientation = 2130771968;
+			
+			// aapt resource value: 0x7f010004
+			public const int rotatable = 2130771972;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -41,11 +56,106 @@ namespace Android
 			}
 		}
 		
-		public partial class Id
+		public partial class Color
 		{
 			
 			// aapt resource value: 0x7f060000
-			public const int startScanningButton = 2131099648;
+			public const int mbButtonBackgroundDisabled = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int mbButtonBackgroundNormal = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int mbButtonBackgroundPressed = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int mbButtonBorder = 2131099651;
+			
+			// aapt resource value: 0x7f060004
+			public const int mbButtonForeground = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int statusBackground = 2131099653;
+			
+			// aapt resource value: 0x7f060006
+			public const int statusForeground = 2131099654;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int lightoff = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int lighton = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int microblink_button = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int rounded_border = 2130837507;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f08000c
+			public const int defaultBackButton = 2131230732;
+			
+			// aapt resource value: 0x7f08000a
+			public const int defaultStatusTextView = 2131230730;
+			
+			// aapt resource value: 0x7f08000b
+			public const int defaultTorchButton = 2131230731;
+			
+			// aapt resource value: 0x7f080009
+			public const int default_photopay_viewfinder = 2131230729;
+			
+			// aapt resource value: 0x7f080004
+			public const int fill = 2131230724;
+			
+			// aapt resource value: 0x7f080005
+			public const int fit = 2131230725;
+			
+			// aapt resource value: 0x7f080000
+			public const int inversePortrait = 2131230720;
+			
+			// aapt resource value: 0x7f080001
+			public const int landscapeLeft = 2131230721;
+			
+			// aapt resource value: 0x7f080002
+			public const int landscapeRight = 2131230722;
+			
+			// aapt resource value: 0x7f080006
+			public const int my_scan_root = 2131230726;
+			
+			// aapt resource value: 0x7f080003
+			public const int portrait = 2131230723;
+			
+			// aapt resource value: 0x7f080007
+			public const int recognizerView = 2131230727;
+			
+			// aapt resource value: 0x7f080008
+			public const int startScanningButton = 2131230728;
 			
 			static Id()
 			{
@@ -60,8 +170,14 @@ namespace Android
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int activity_blinkid_scan = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int Main = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int overlay_blinkid_scan = 2130968578;
 			
 			static Layout()
 			{
@@ -76,8 +192,8 @@ namespace Android
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -92,8 +208,8 @@ namespace Android
 		public partial class Raw
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int beep = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int beep = 2131034112;
 			
 			static Raw()
 			{
@@ -108,11 +224,35 @@ namespace Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int app_name = 2131034112;
+			// aapt resource value: 0x7f070000
+			public const int Align = 2131165184;
 			
-			// aapt resource value: 0x7f050001
-			public const int hello = 2131034113;
+			// aapt resource value: 0x7f070001
+			public const int app_name = 2131165185;
+			
+			// aapt resource value: 0x7f070002
+			public const int btnOK = 2131165186;
+			
+			// aapt resource value: 0x7f070003
+			public const int error = 2131165187;
+			
+			// aapt resource value: 0x7f070004
+			public const int errorDesc = 2131165188;
+			
+			// aapt resource value: 0x7f070009
+			public const int hello = 2131165193;
+			
+			// aapt resource value: 0x7f070005
+			public const int mbHelp = 2131165189;
+			
+			// aapt resource value: 0x7f070006
+			public const int mbHome = 2131165190;
+			
+			// aapt resource value: 0x7f070007
+			public const int mbLightOff = 2131165191;
+			
+			// aapt resource value: 0x7f070008
+			public const int mbLightOn = 2131165192;
 			
 			static String()
 			{
@@ -120,6 +260,45 @@ namespace Android
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] RecognizerView = new int[]
+			{
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971};
+			
+			// aapt resource value: 1
+			public const int RecognizerView_animateRotation = 1;
+			
+			// aapt resource value: 2
+			public const int RecognizerView_animationDuration = 2;
+			
+			// aapt resource value: 3
+			public const int RecognizerView_aspectMode = 3;
+			
+			// aapt resource value: 0
+			public const int RecognizerView_initialOrientation = 0;
+			
+			public static int[] RecognizerView_LayoutParams = new int[]
+			{
+					2130771972};
+			
+			// aapt resource value: 0
+			public const int RecognizerView_LayoutParams_rotatable = 0;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
