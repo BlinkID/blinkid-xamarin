977aa362dcb6d49ee28fbd8fc32a80256f926bba
Nenad Mikša <nenad.miksa@microblink.com>
2018-07-04 19:43:04 +0200

    added USDL combined recognizer with implementations

---
+++ Binding/Forms/BlinkID.Forms/BlinkID.Forms.Android/Recognizers/Implementations/UsdlCombinedRecognizer.cs
@@ -0,0 +1,66 @@
+﻿using Microblink.Forms.Core.Recognizers;
+using Microblink.Forms.Droid.Recognizers;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(UsdlCombinedRecognizer))]
+namespace Microblink.Forms.Droid.Recognizers
+{
+    public sealed class UsdlCombinedRecognizer : Recognizer, IUsdlCombinedRecognizer
+    {
+        Com.Microblink.Entities.Recognizers.Blinkid.Usdl.UsdlCombinedRecognizer nativeRecognizer;
+        UsdlCombinedRecognizerResult result;
+
+        public UsdlCombinedRecognizer()
+            : base(new Com.Microblink.Entities.Recognizers.Blinkid.Usdl.UsdlCombinedRecognizer())
+        {
+            nativeRecognizer = NativeRecognizer as Com.Microblink.Entities.Recognizers.Blinkid.Usdl.UsdlCombinedRecognizer;
+            result = new UsdlCombinedRecognizerResult(nativeRecognizer.GetResult() as Com.Microblink.Entities.Recognizers.Blinkid.Usdl.UsdlCombinedRecognizer.Result);
+        }
+
+        public override IRecognizerResult BaseResult => result;
+
+        public IUsdlCombinedRecognizerResult Result => result;
+
+        public uint FaceImageDpi { get => (uint)nativeRecognizer.FaceImageDpi; set => nativeRecognizer.FaceImageDpi = (int)value; }
+        public bool ReturnFaceImage { get => nativeRecognizer.ShouldReturnFaceImage(); set => nativeRecognizer.SetReturnFaceImage(value); }
+        public uint FullDocumentImageDpi { get => (uint)nativeRecognizer.FullDocumentImageDpi; set => nativeRecognizer.FullDocumentImageDpi = (int)value; }
+        public bool ReturnFullDocumentImage { get => nativeRecognizer.ShouldReturnFullDocumentImage(); set => nativeRecognizer.SetReturnFullDocumentImage(value); }
+        public bool SignResult { get => nativeRecognizer.ShouldSignResult(); set => nativeRecognizer.SetSignResult(value); }
+    }
+
+    public sealed class UsdlCombinedRecognizerResult : RecognizerResult, IUsdlCombinedRecognizerResult
+    {
+        Com.Microblink.Entities.Recognizers.Blinkid.Usdl.UsdlCombinedRecognizer.Result nativeResult;
+
+        internal UsdlCombinedRecognizerResult(Com.Microblink.Entities.Recognizers.Blinkid.Usdl.UsdlCombinedRecognizer.Result nativeResult)
+            : base(nativeResult)
+        {
+            this.nativeResult = nativeResult;
+        }
+
+        public bool Uncertain => nativeResult.IsUncertain;
+
+        public string RawStringData => nativeResult.RawStringData;
+
+        public byte[] RawData => nativeResult.GetRawData();
+
+        public string[] OptionalElements => nativeResult.GetOptionalElements();
+
+        public byte[] DigitalSignature => nativeResult.GetDigitalSignature();
+
+        public int DigitalSignatureVersion => nativeResult.DigitalSignatureVersion;
+
+        public bool DocumentDataMatch => nativeResult.IsDocumentDataMatch;
+
+        public ImageSource FaceImage => Utils.ConvertAndroidBitmap(nativeResult.FaceImage.ConvertToBitmap());
+
+        public ImageSource FullDocumentImage => Utils.ConvertAndroidBitmap(nativeResult.FullDocumentImage.ConvertToBitmap());
+
+        public bool ScanningFirstSideDone => nativeResult.IsScanningFirstSideDone;
+
+        public string GetField(UsdlKeys key)
+        {
+            return nativeResult.GetField(Com.Microblink.Entities.Recognizers.Blinkbarcode.Usdl.UsdlKeys.Values()[(int)key]);
+        }
+    }
+}
---
+++ Binding/Forms/BlinkID.Forms/BlinkID.Forms.Core/Recognizers/Concrete/IUsdlCombinedRecognizer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+namespace Microblink.Forms.Core.Recognizers
+{
+    public interface IUsdlCombinedRecognizer : IRecognizer
+    {
+        /// <summary>
+        /// Gets or sets the face image DPI. Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception.
+        /// Default value is <c>250</c>
+        /// </summary>
+        /// <value>The face image dpi.</value>
+        uint FaceImageDpi { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether face image from ID card should be extracted
+        /// Default value is <c>false</c>
+        /// </summary>
+        /// <value><c>true</c> if return face image; otherwise, <c>false</c>.</value>
+        bool ReturnFaceImage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full document image DPI. Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception.
+        /// Default value is <c>250</c>
+        /// </summary>
+        /// <value>The full document image dpi.</value>
+        uint FullDocumentImageDpi { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether full document image of ID card should be extracted
+        /// Default value is <c>false</c>
+        /// </summary>
+        /// <value><c>true</c> if return face image; otherwise, <c>false</c>.</value>
+        bool ReturnFullDocumentImage { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether recognition result should be signed.
+        /// </summary>
+        /// <value><c>true</c> if sign result; otherwise, <c>false</c>.</value>
+        bool SignResult { get; set; }
+
+        /// <summary>
+        /// Gets the result.
+        /// </summary>
+        /// <value>The result.</value>
+        IUsdlCombinedRecognizerResult Result { get; }
+    }
+
+    public interface IUsdlCombinedRecognizerResult : IRecognizerResult
+    {
+        /// <summary>
+        /// Gets the digital signature of the recognition result. Available only if enabled with SignResult property. 
+        /// </summary>
+        /// <value>The digital signature.</value>
+        byte[] DigitalSignature { get; }
+
+        /// <summary>
+        /// Gets the version of the digital signature. Available only if enabled with signResult property. 
+        /// </summary>
+        /// <value>The digital signature version.</value>
+        int DigitalSignatureVersion { get; }
+
+        /// <summary>
+        /// Returns true if data from scanned parts/sides of the document match,
+        /// false otherwise. For example if date of expiry is scanned from the front and back side
+        /// of the document and values do not match, this method will return false. Result will
+        /// be true only if scanned values for all fields that are compared are the same. 
+        /// </summary>
+        /// <value><c>true</c> if document data match; otherwise, <c>false</c>.</value>
+        bool DocumentDataMatch { get; }
+
+        /// <summary>
+        /// Gets the face image from the document if enabled with ReturnFaceImage property. 
+        /// </summary>
+        /// <value>The face image.</value>
+        Xamarin.Forms.ImageSource FaceImage { get; }
+
+        /// <summary>
+        /// Gets the full document image if enabled with returnFullDocumentImage property. 
+        /// </summary>
+        /// <value>The full document image.</value>
+        Xamarin.Forms.ImageSource FullDocumentImage { get; }
+
+        /// <summary>
+        /// Returns true if recognizer has finished scanning first side and is now scanning back side,
+        /// false if it's still scanning first side. 
+        /// </summary>
+        /// <value><c>true</c> if scanning first side done; otherwise, <c>false</c>.</value>
+        bool ScanningFirstSideDone { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this result is uncertain, i.e. if scanned barcode 
+        /// was incomplete (i.e. (has parts of it missing).
+        /// </summary>
+        /// <value><c>true</c> if uncertain; otherwise, <c>false</c>.</value>
+        bool Uncertain { get; }
+
+        /// <summary>
+        /// Gets the raw string data inside 2D barcode.
+        /// </summary>
+        /// <value>The raw string data.</value>
+        string RawStringData { get; }
+
+        /// <summary>
+        /// Gets the raw data bytes contained inside 2D barcode.
+        /// </summary>
+        /// <value>The raw data.</value>
+        byte[] RawData { get; }
+
+        /// <summary>
+        /// Gets the array of elements that are not part of AAMVA standard and are specific to each US state.
+        /// If no specific elements existed inside 2D barcode, this is an empty array.Otherwise,
+        /// this array contains list of state-specific elements in the same order as given inside
+        /// barcode.
+        /// NOTE: Size of this array is both state-specific and barcode-specific.Each US state has
+        /// ability to arbitrarily define size and contents of these elements.You can obtain the
+        /// US state with getField(UsdlKeys) and using UsdlKeys.IssuingJurisdictionName
+        /// as a parameter.
+        /// </summary>
+        /// <value>The optional elements.</value>
+        string[] OptionalElements { get; }
+
+        /// <summary>
+        /// Gets the field inside US Driver's licence. Available Keys are listed in UsdlKeys enum.
+        /// </summary>
+        /// <returns>The field.</returns>
+        /// <param name="key">Key.</param>
+        string GetField(UsdlKeys key);
+    }
+}
