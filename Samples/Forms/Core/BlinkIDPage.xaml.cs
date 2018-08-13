7d6422c259ba6f0e4c152ee4bf5f21c100459167
juraskrlec <jura.skrlec@microblink.com>
2018-08-13 19:01:11 +0200

    Updated Samples and Packages and Core dependencies for v4.1.0

--- Samples/Forms/Core/BlinkIDPage.xaml.cs
+++ Samples/Forms/Core/BlinkIDPage.xaml.cs
@@ -64,7 +64,7 @@ namespace BlinkIDApp
             // both these license keys are demo license keys for bundleID/applicationID com.microblink.xamarin.blinkid
             if (Device.RuntimePlatform == Device.iOS)
             {
-                licenseKey = "sRwAAAEeY29tLm1pY3JvYmxpbmsueGFtYXJpbi5ibGlua2lks3unDF2B9jpa6FeAwxAdkXxaNOMEzJmfZ4hR21AVB8wknhBesyrlSBS0GhBEOmnINIQuUaYt5/35Ed6eOxOyXZeeSVl6eljzTY88HilqzAc4x4L1donsPivtU0wNm1Ew1efXkB4GIEzC4oHzkQDLiFegrSOXhZwxOTya1zIUw537gG/c52NSW67xV7k1ooTfaSK+JgADz3V4Sd4FAHXXNx47WwfV7qMe6cVal/9AtezVn5hocw==";    
+                licenseKey = "sRwAAAEeY29tLm1pY3JvYmxpbmsueGFtYXJpbi5ibGlua2lks3unDF2B9jpa6FeAx5h89osjSFXqQRtWjes0WSEXe3rubgFApHsFdSF2oJZL0MhSL18AgbaLmPiOT3RFu+lqtKtpsjqRfxEVmc9cU98GnjciEeAOC331OUeT5R1p7zKqsenX/3IytQJuMG4ruEaRXc5746y/Kv8ch0XDgv/nRu4siNlvpkjsUjkeyokbUUwi+uzL6t2G4V/sX0SXTMWPjG7zjtW4ovwpGs6fct/dpBJQfzRQNCn9kg==";    
             }
             else
             {
