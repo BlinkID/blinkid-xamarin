0e33b69bb4682011c96ee98b1228551d05268ccd
Ivan Grce <ivan.grce@microblink.com>
2018-09-04 16:12:53 +0200

    [android] updated samples for v4.2.0

--- Samples/Forms/Core/BlinkIDPage.xaml.cs
+++ Samples/Forms/Core/BlinkIDPage.xaml.cs
@@ -68,7 +68,7 @@ namespace BlinkIDApp
             }
             else
             {
-                licenseKey = "sRwAAAAeY29tLm1pY3JvYmxpbmsueGFtYXJpbi5ibGlua2lke7qv4oIhH4ywlU8/ZIc8ttT74daTKaSlmN1khLj7e+CR+5SB1pjJQuNbEIlPAVl67d85JYqkRXxuWzKbaSWuPI7MzpKrXphakcSuyLYATcHaZMHh2rJvFFfYWXEWz6GZJSRk4TZmdqWBsjdNEJBe2F8hb/CEuKgkZkoESgLsCEYWf6Se+IkRBhRcmmOrcqJAf5YzhNbhlClR7GmNEPZheayI+i784TvIAnHFGloQF52KSF8YFm3dTw==";
+                licenseKey = "sRwAAAAeY29tLm1pY3JvYmxpbmsueGFtYXJpbi5ibGlua2lke7qv4oIhH4ywlU8/Ykcxtro0niv3z3MEx6zurAEm+re10In/l3kLnW/R/vf1JI3weawIMA/ZrsUteLBNgoxST1uM6/g0UBZk5LNiIUHsl1Vgk15fZHJ42o+VAq40Iv6NKOw0yc71eMucbppuSgkH4hTNiR/hOUO/yZsc+GGN1q7Sh+7yCaDfnAMY8yx4z9plZEeQdQSnhXzb9x9ofYabsp2sN4qz/BedEi9+X3C+TTw4HrMoyZ4pCg==";
             }
 
             // since DependencyService requires implementations to have default constructor, a factory is needed
