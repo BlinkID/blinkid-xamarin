7c46a7f6225250b5ff52b9d3897453aae894f108
Ivan Grce <ivan.grce@microblink.com>
2019-02-21 15:13:08 +0100

    [Forms.Core, Forms.Android] added BlinkCard Overlay

---
+++ Binding/Forms/BlinkID.Forms/BlinkID.Forms.Android/Overlays/Implementations/BlinkCardOverlaySettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Com.Microblink.Uisettings;
+using Microblink.Forms.Core.Overlays;
+using Microblink.Forms.Core.Recognizers;
+using Microblink.Forms.Droid.Overlays.Implementations;
+using Microblink.Forms.Droid.Recognizers;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BlinkCardOverlaySettingsFactory))]
+namespace Microblink.Forms.Droid.Overlays.Implementations
+{
+    public sealed class BlinkCardOverlaySettings : OverlaySettings, IBlinkCardOverlaySettings
+    {
+        public override UISettings NativeUISettings { 
+            get {
+                var concreteUISettings = (BlinkCardUISettings)base.NativeUISettings;
+                if (FirstSideInstructions != null) {
+                    concreteUISettings.FirstSideInstructions = FirstSideInstructions;
+                }
+                if (SecondSideInstructions != null) {
+                    concreteUISettings.SecondSideInstructions = SecondSideInstructions;
+                }
+                return concreteUISettings;
+            }
+        }
+
+        public string FirstSideInstructions { get; set; }
+
+        public string SecondSideInstructions { get; set; }
+
+        public BlinkCardOverlaySettings(IRecognizerCollection recognizerCollection)
+            : base(new BlinkCardUISettings((recognizerCollection as RecognizerCollection).NativeRecognizerBundle), recognizerCollection)
+        {}
+    }
+
+    public sealed class BlinkCardOverlaySettingsFactory : IBlinkCardOverlaySettingsFactory
+    {
+        public IBlinkCardOverlaySettings CreateBlinkCardOverlaySettings(IRecognizerCollection recognizerCollection)
+        {
+            return new BlinkCardOverlaySettings(recognizerCollection);
+        }
+    }
+}
