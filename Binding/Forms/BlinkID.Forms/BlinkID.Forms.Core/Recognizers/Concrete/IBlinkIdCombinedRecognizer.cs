7f0bb5a3306c5667482002e44b7d7820d09b96ac
Jura Skrlec <jura.skrlec@microblink.com>
2021-02-19 09:37:22 +0100

    [Forms.Core] Updated interfaces v5.10.0

--- Binding/Forms/BlinkID.Forms/BlinkID.Forms.Core/Recognizers/Concrete/IBlinkIdCombinedRecognizer.cs
+++ Binding/Forms/BlinkID.Forms/BlinkID.Forms.Core/Recognizers/Concrete/IBlinkIdCombinedRecognizer.cs
@@ -1,125 +1,169 @@
 ﻿namespace Microblink.Forms.Core.Recognizers
 {
     /// <summary>
-    /// A generic recognizer which can scan front and back side of the document.
+    /// Recognizer which can scan front and back side of the United States driver license.
     /// </summary>
     public interface IBlinkIdCombinedRecognizer : IRecognizer
     {
         
         /// <summary>
-        /// Defines whether blured frames filtering is allowed. 
+        /// Defines whether blured frames filtering is allowed
+        /// 
+        ///  
         ///
         /// By default, this is set to 'true'
         /// </summary>
         bool AllowBlurFilter { get; set; }
         
         /// <summary>
-        /// Defines whether returning of unparsed MRZ (Machine Readable Zone) results is allowed. 
+        /// Defines whether returning of unparsed MRZ (Machine Readable Zone) results is allowed
+        /// 
+        ///  
         ///
         /// By default, this is set to 'false'
         /// </summary>
         bool AllowUnparsedMrzResults { get; set; }
         
         /// <summary>
-        /// Defines whether returning unverified MRZ (Machine Readable Zone) results is allowed. 
+        /// Defines whether returning unverified MRZ (Machine Readable Zone) results is allowed
+        /// Unverified MRZ is parsed, but check digits are incorrect
+        /// 
+        ///  
         ///
         /// By default, this is set to 'true'
         /// </summary>
         bool AllowUnverifiedMrzResults { get; set; }
         
         /// <summary>
-        /// Whether sensitive data should be removed from images, result fields or both. 
+        /// Defines whether sensitive data should be removed from images, result fields or both.
+        /// The setting only applies to certain documents
+        /// 
+        ///  
         ///
-        /// By default, this is set to 'FullResult'
+        /// By default, this is set to 'MBAnonymizationModeFullResult'
         /// </summary>
         AnonymizationMode AnonymizationMode { get; set; }
         
         /// <summary>
-        /// The DPI (Dots Per Inch) for face image that should be returned. 
+        /// Property for setting DPI for face images
+        /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
+        /// 
+        ///  
         ///
         /// By default, this is set to '250'
         /// </summary>
         int FaceImageDpi { get; set; }
         
         /// <summary>
-        /// The DPI (Dots Per Inch) for full document image that should be returned. 
+        /// Property for setting DPI for full document images
+        /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
+        /// 
+        ///  
         ///
         /// By default, this is set to '250'
         /// </summary>
         int FullDocumentImageDpi { get; set; }
         
         /// <summary>
-        /// The extension factors for full document image. 
+        /// Image extension factors for full document image.
+        /// 
+        /// @see ImageExtensionFactors
+        ///  
         ///
-        /// By default, this is set to '[0.0, 0.0, 0.0, 0.0]'
+        /// By default, this is set to '{0.0f, 0.0f, 0.0f, 0.0f}'
         /// </summary>
         IImageExtensionFactors FullDocumentImageExtensionFactors { get; set; }
         
         /// <summary>
-        /// Padding is a minimum distance from the edge of the frame and it is defined 
+        /// Pading is a minimum distance from the edge of the frame and is defined as a percentage of the frame width. Default value is 0.0f and in that case
+        /// padding edge and image edge are the same.
+        /// Recommended value is 0.02f.
+        /// 
+        ///  
         ///
-        /// By default, this is set to '0.0'
+        /// By default, this is set to '0.0f'
         /// </summary>
         float PaddingEdge { get; set; }
         
         /// <summary>
-        /// Currently set recognition mode filter. 
+        /// Enable or disable recognition of specific document groups supported by the current license.
+        /// 
+        ///  
         ///
-        /// By default, this is set to '[true, true, true, true, true]'
+        /// By default, this is set to 'all modes are enabled'
         /// </summary>
         IRecognitionModeFilter RecognitionModeFilter { get; set; }
         
         /// <summary>
-        /// Defines whether face image will be available in result. 
+        /// Sets whether face image from ID card should be extracted
+        /// 
+        ///  
         ///
         /// By default, this is set to 'false'
         /// </summary>
         bool ReturnFaceImage { get; set; }
         
         /// <summary>
-        /// Defines whether full document image will be available in 
+        /// Sets whether full document image of ID card should be extracted.
+        /// 
+        ///  
         ///
         /// By default, this is set to 'false'
         /// </summary>
         bool ReturnFullDocumentImage { get; set; }
         
         /// <summary>
-        /// Defines whether signature image will be available in result. 
+        /// Sets whether signature image from ID card should be extracted.
+        /// 
+        ///  
         ///
         /// By default, this is set to 'false'
         /// </summary>
         bool ReturnSignatureImage { get; set; }
         
         /// <summary>
-        /// Configure the recognizer to only work on already cropped and dewarped images. 
+        /// Configure the recognizer to only work on already cropped and dewarped images.
+        /// This only works for still images - video feeds will ignore this setting.
+        /// 
+        ///  
         ///
-        /// By default, this is set to 'true'
+        /// By default, this is set to 'false'
         /// </summary>
         bool ScanCroppedDocumentImage { get; set; }
         
         /// <summary>
-        /// Defines whether or not recognition result should be signed. 
+        /// Whether or not recognition result should be signed.
+        /// 
+        ///  
         ///
         /// By default, this is set to 'false'
         /// </summary>
         bool SignResult { get; set; }
         
         /// <summary>
-        /// The DPI (Dots Per Inch) for signature image that should be returned. 
+        /// Property for setting DPI for signature images
+        /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
+        /// 
+        ///  
         ///
         /// By default, this is set to '250'
         /// </summary>
         int SignatureImageDpi { get; set; }
         
         /// <summary>
-        /// Skip back side capture and processing step when back side of the document is not supported. 
+        /// Skip back side capture and processing step when back side of the document is not supported
+        /// 
+        ///  
         ///
         /// By default, this is set to 'false'
         /// </summary>
         bool SkipUnsupportedBack { get; set; }
         
         /// <summary>
-        /// Whether result characters validatation is performed. 
+        /// Defines whether result characters validatation is performed.
+        /// If a result member contains invalid character, the result state cannot be valid
+        /// 
+        ///  
         ///
         /// By default, this is set to 'true'
         /// </summary>
@@ -138,7 +182,7 @@
     public interface IBlinkIdCombinedRecognizerResult : IRecognizerResult {
         
         /// <summary>
-        /// The additional name information of the document owner. 
+        /// The additional address information of the document owner. 
         /// </summary>
         string AdditionalAddressInformation { get; }
         
@@ -153,27 +197,29 @@
         string Address { get; }
         
         /// <summary>
-        /// The current age of the document owner in years. It is calculated difference 
+        /// The current age of the document owner in years. It is calculated difference
+        /// between now and date of birth. Now is current time on the device.
+        /// @return current age of the document owner in years or -1 if date of birth is unknown. 
         /// </summary>
         int Age { get; }
         
         /// <summary>
-        /// Image analysis result for the scanned document back side image 
+        /// Defines possible color and moire statuses determined from scanned back image. 
         /// </summary>
         IImageAnalysisResult BackImageAnalysisResult { get; }
         
         /// <summary>
-        /// The data extracted from the back side visual inspection zone. 
+        /// Defines the data extracted from the back side visual inspection zone. 
         /// </summary>
         IVizResult BackVizResult { get; }
         
         /// <summary>
-        /// The data extracted from the barcode. 
+        /// Defines the data extracted from the barcode. 
         /// </summary>
         IBarcodeResult BarcodeResult { get; }
         
         /// <summary>
-        /// The document class information. 
+        /// The classification information. 
         /// </summary>
         IClassInfo ClassInfo { get; }
         
@@ -198,12 +244,12 @@
         IDate DateOfIssue { get; }
         
         /// <summary>
-        /// Defines digital signature of recognition results. 
+        /// Digital signature of the recognition result. Available only if enabled with signResult property. 
         /// </summary>
         byte[] DigitalSignature { get; }
         
         /// <summary>
-        /// Defines digital signature version. 
+        /// Version of the digital signature. Available only if enabled with signResult property. 
         /// </summary>
         int DigitalSignatureVersion { get; }
         
@@ -213,7 +259,10 @@
         string DocumentAdditionalNumber { get; }
         
         /// <summary>
-        /// Defines result of the data matching algorithm for scanned parts/sides of the document. 
+        /// Returns DataMatchResultSuccess if data from scanned parts/sides of the document match,
+        /// DataMatchResultFailed otherwise. For example if date of expiry is scanned from the front and back side
+        /// of the document and values do not match, this method will return DataMatchResultFailed. Result will
+        /// be DataMatchResultSuccess only if scanned values for all fields that are compared are the same. 
         /// </summary>
         DataMatchResult DocumentDataMatch { get; }
         
@@ -238,12 +287,18 @@
         string Employer { get; }
         
         /// <summary>
-        /// Checks whether the document has expired or not by comparing the current 
+        /// Checks whether the document has expired or not by comparing the current
+        /// time on the device with the date of expiry.
+        /// 
+        /// @return true if the document has expired, false in following cases:
+        /// document does not expire (date of expiry is permanent)
+        /// date of expiry has passed
+        /// date of expiry is unknown and it is not permanent 
         /// </summary>
         bool Expired { get; }
         
         /// <summary>
-        /// Face image from the document 
+        /// face image from the document if enabled with returnFaceImage property. 
         /// </summary>
         Xamarin.Forms.ImageSource FaceImage { get; }
         
@@ -253,22 +308,22 @@
         string FirstName { get; }
         
         /// <summary>
-        /// Image analysis result for the scanned document front side image 
+        /// Defines possible color and moire statuses determined from scanned front image. 
         /// </summary>
         IImageAnalysisResult FrontImageAnalysisResult { get; }
         
         /// <summary>
-        /// The data extracted from the front side visual inspection zone. 
+        /// Defines the data extracted from the front side visual inspection zone. 
         /// </summary>
         IVizResult FrontVizResult { get; }
         
         /// <summary>
-        /// Back side image of the document 
+        /// back side image of the document if enabled with returnFullDocumentImage property. 
         /// </summary>
         Xamarin.Forms.ImageSource FullDocumentBackImage { get; }
         
         /// <summary>
-        /// Front side image of the document 
+        /// front side image of the document if enabled with returnFullDocumentImage property. 
         /// </summary>
         Xamarin.Forms.ImageSource FullDocumentFrontImage { get; }
         
@@ -298,7 +353,7 @@
         string MaritalStatus { get; }
         
         /// <summary>
-        /// The data extracted from the machine readable zone. 
+        /// The data extracted from the machine readable zone 
         /// </summary>
         IMrzResult MrzResult { get; }
         
@@ -318,7 +373,7 @@
         string PlaceOfBirth { get; }
         
         /// <summary>
-        /// Status of the last recognition process. 
+        /// Defines status of the last recognition process. 
         /// </summary>
         ProcessingStatus ProcessingStatus { get; }
         
@@ -348,7 +403,8 @@
         string ResidentialStatus { get; }
         
         /// <summary>
-        /// {true} if recognizer has finished scanning first side and is now scanning back side, 
+        /// Returns true if recognizer has finished scanning first side and is now scanning back side,
+        /// false if it's still scanning first side. 
         /// </summary>
         bool ScanningFirstSideDone { get; }
         
@@ -358,7 +414,7 @@
         string Sex { get; }
         
         /// <summary>
-        /// Signature image from the document 
+        /// image of the signature if enabled with returnSignatureImage property. 
         /// </summary>
         Xamarin.Forms.ImageSource SignatureImage { get; }
         
